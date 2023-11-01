8be2e392641662a97fb51970bc701c6237f2ddc7
Michael Whittle <michael@lifecycle-ps.com>
2023-11-01 11:01:07 +0000

    finished bootstrap data tables for exchanges

--- app.py
+++ app.py
@@ -1,6 +1,43 @@
+import re
+import sys
+import argparse
 from flask import Flask, render_template
 from api.financial_data import EODHDAPIsDataFetcher
 from config import API_TOKEN
+
+
+parser = argparse.ArgumentParser(description="EODHD APIs")
+parser.add_argument(
+    "--host",
+    type=str,
+    help="Web service IP (default: 127.0.0.1)",
+)
+parser.add_argument(
+    "--port",
+    type=int,
+    help="Web service port (default: 5000)",
+)
+parser.add_argument("--debug", action="store_true", help="Enable debugging")
+args = parser.parse_args()
+
+
+# Listen on local host
+http_host = "0.0.0.0"
+if args.host is not None:
+    p = re.compile(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+    if p.match(args.host):
+        http_host = args.host
+    else:
+        parser.print_help(sys.stderr)
+
+# Listen on local port
+http_port = 5000
+if args.port is not None:
+    if args.port >= 0 and args.port <= 65535:
+        http_port = args.port
+    else:
+        parser.print_help(sys.stderr)
+
 
 app = Flask(__name__)
 
@@ -14,4 +51,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host=http_host, port=http_port, debug=args.debug)
